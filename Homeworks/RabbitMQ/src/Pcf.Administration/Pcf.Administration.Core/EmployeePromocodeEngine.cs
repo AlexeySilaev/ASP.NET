f02b4d2f28cb8c4e70f93cb525bc916412da27df
Силаев Алексей <flownder@mail.ru>
2026-06-28 11:02:47 +0300

    ДЗ по RabbitMQ

---
+++ Homeworks/RabbitMQ/src/Pcf.Administration/Pcf.Administration.Core/EmployeePromocodeEngine.cs
@@ -0,0 +1,31 @@
+﻿using Pcf.Administration.Core.Abstractions.Repositories;
+using Pcf.Administration.Core.Domain.Administration;
+using System;
+using System.Threading.Tasks;
+
+namespace Pcf.Administration.Core
+{
+    public class EmployeePromocodeEngine
+    {
+        private readonly IRepository<Employee> _employeeRepository;
+
+        public EmployeePromocodeEngine(IRepository<Employee> employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<bool> ApplyAsync(Guid employeeId)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(employeeId);
+
+            if (employee == null)
+                return false;
+
+            employee.AppliedPromocodesCount++;
+
+            await _employeeRepository.UpdateAsync(employee);
+
+            return true;
+        }
+    }
+}
---
+++ Homeworks/RabbitMQ/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.WebHost/MassTransit/PromocodeConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Pcf.GivingToCustomer.Core;
+using Pcf.GivingToCustomer.WebHost.Mappers;
+using Pcf.GivingToCustomer.WebHost.Models;
+using SharedModels;
+using System.Threading.Tasks;
+
+namespace Pcf.GivingToCustomer.WebHost.MassTransit
+{
+    public class PromocodeConsumer : IConsumer<IGivePromoCodeToCustomerDto>
+    {
+        private readonly PromoCodeEngine _promoCodeEngine;
+
+        public PromocodeConsumer(PromoCodeEngine promoCodeEngine)
+        {
+            _promoCodeEngine = promoCodeEngine;
+        }
+
+        public async Task Consume(ConsumeContext<IGivePromoCodeToCustomerDto> context)
+        {
+            IGivePromoCodeToCustomerDto request = context.Message;
+            bool result = await _promoCodeEngine.ApplyAsync(request.PreferenceId,
+                (preference, customers) => PromoCodeMapper.MapFromModel(request, preference, customers));
+        }
+    }
+}
